25e399a469d33a014c1eae0ccee8e24547e97f80
namjae <woslek123@naver.com>
2025-09-25 16:15:04 +0900

    hello coding.1

---
+++ C#hellocoding/MYFirstProgram/MYFirstProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WriteLine을 쓰면");
+            Console.WriteLine("한 줄씩 나옵니다.");
+            Console.Write("Write를 쓰면 아닙니다.");
+            Console.Write("이어져서 나오죠?");
+            Console.WriteLine("Write 뒤에 WriteLine을 썼습니다.");
+            // Console.Write () 함수는 출력 뒤에 새줄을 만들지 않는다.
+
+            Console.Write("바구니 안에 담긴 사과의 개수:");
+            Console.WriteLine(12);
+
+            Console.Write("사과 바구니의 무게:");
+            Console.WriteLine(1.32);
+            // 숫자의 경우 "" '' 필요없지만 특수문자는 필요함
+            Console.WriteLine("!!?#$%");
+
+            Console.WriteLine("큰따옴표\" 출력");//큰따옴표 출력 ( \ 줄바꿈 파이썬과 동일)
+                                           //큰 따옴표를 출력하려고 시도
+            Console.WriteLine("\\\'\"");
+        }
+    }
+}
---
+++ C#hellocoding/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UltimateBaseball
+{
+    class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------------------------------------------+");
+            Console.WriteLine("|              궁국의  숫자야구  게임                   |");
+
+        }
+            }
+}
